456926bc0abf47f05c34823f407e58c2a0782db3
alfiluthfan <alfiluthfan26@gmail.com>
2024-01-09 23:05:42 +0700

    Change product title

--- add_product.php
+++ add_product.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UnitedKU | Add Product</title>
+    <title>UnitedKU | Add Data Product</title>
     <link rel="icon" href="src/emyu.png" type="image/icon type">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
     <link rel="stylesheet" href="style/product_list.css" />
